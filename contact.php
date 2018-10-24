44d7473484d9783cd2ed49bc78827667dd75d200
Sarah G <savigoma@gmail.com>
2018-10-24 13:38:42 -0600

    final interim portfolio design

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Sarah V G&oacute;mez | Contact Information</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet"  href="styles.css">
+	</head>
+	<body>
+        <header>
+        	<h1><a href="index.php">Sarah V G&oacute;mez</a> | <span>selected works</span></h1>
+		<nav>
+			<a href="">Resume</a>
+			<a href="contact.php">Contact</a>
+		</nav>
+        </header>
+
+	<main class="contact">
+
+        <section>
+		<h2>Contact Information</h2>
+		<p>sarahvgomez [at] gmail [dot] com<br>
+		sarahvgomez.com<br>
+		codepen.io/saraconache<br>
+		github.com/saraconache</p>
+    </section>
+
+	</main>
+	<footer><p>Copyright &copy; 2018 Sarah G&oacute;mez. All rights reserved.</p></footer>
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -2,35 +2,33 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Sarah V Gómez</title>
+		<title>Sarah V G&oacute;mez</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet"  href="styles.css">
 	</head>
 	<body>
         <header>
-        	<h1>SARAH V GOMEZ |</h1>
-	        <h2>selected works</h2>
+        	<h1><a href="index.php">Sarah V G&oacute;mez</a> | <span>selected works</span></h1>
+		<nav>
+			<a href="">Resume</a>
+			<a href="contact.php">Contact</a>
+		</nav>
         </header>
 
-
-
-	<main class="blog-slider__container">
-		<div class="blogSlides fade">
-		  <!-- <div class="blog-slider__numbertext">1 / 3</div> -->
+	<main class="workContainer">
+		<div class="slides fade">
 		  <img src="images/Branding_AFCC.png">
-		  <div class="blog-slider__text">Caption Text</div>
+		  <div class="caption">Logo Proposal for AFFC rebranding.</div>
 		</div>
 	      
-		<div class="blogSlides fade">
-		  <!-- <div class="blog-slider__numbertext">2 / 3</div> -->
+		<div class="slides fade">
 		  <img src="images/Branding_mynte.png">
-		  <div class="blog-slider__text">Caption Two</div>
+		  <div class="caption">Logo for Marketing Company (academic exercise)</div>
 		</div>
 	      
-		<div class="blogSlides fade">
-		  <!-- <div class="blog-slider__numbertext">3 / 3</div> -->
+		<div class="slides fade">
 		  <img src="images/Webdesign_peachnteal.png">
-		  <div class="blog-slider__text">Caption Three</div>
+		  <div class="caption">Web design &amp; development for Peach &amp; Teal</div>
 		</div>
 	      <div>
 		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -38,12 +36,13 @@
 	      </div>
 	      <br>
 	      
-	      <div class="blogSlider__dots" style="text-align:center">
-		<span class="blogSlider__dots--dot" onclick="currentSlide(1)"></span> 
-		<span class="blogSlider__dots--dot" onclick="currentSlide(2)"></span> 
-		<span class="blogSlider__dots--dot" onclick="currentSlide(3)"></span> 
+	      <div class="dotsContainer" style="text-align:center">
+		<span class="dotsContainer--dot" onclick="currentSlide(1)"></span> 
+		<span class="dotsContainer--dot" onclick="currentSlide(2)"></span> 
+		<span class="dotsContainer--dot" onclick="currentSlide(3)"></span> 
 	      </div>
 	</main>
+	<footer><p>Copyright &copy; 2018 Sarah G&oacute;mez. All rights reserved.</p></footer>
 
 
 <script>
@@ -60,8 +59,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("blogSlides");
-  var dots = document.getElementsByClassName("blogSlider__dots--dot");
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dotsContainer--dot");
   if (n > slides.length) {
     slideIndex = 1
   }
